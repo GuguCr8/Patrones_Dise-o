4760fdbd6e932cf780566681a558eb4f9f566a2b
cristianrojas <cristianrojas@imacdecristian.home>
2019-01-18 13:47:07 +0100

    Patron completo salvo error en la Prueba

---
+++ patronesdediseño/src/patronesdediseño/PruebaPatrones.java
@@ -0,0 +1,15 @@
+package patronesdediseño;
+
+import patronesdediseño.Strategy.AvionComercial;
+
+public class PruebaPatrones {
+	
+	public static void main(String[]args){
+		AvionComercial avionComercial = new AvionComercial();
+		AvionRapido avionRapido = new AvionRapido();
+		
+		
+	}
+
+}
+ 
--- patronesdediseño/src/patronesdediseño/Strategy.java
+++ patronesdediseño/src/patronesdediseño/Strategy.java
@@ -1,9 +1,63 @@
 package patronesdediseño;
 
+/* Define una familia de algoritmos intercambiables, los cuales permiten variar su compor
+ * sin importar el cliente que los use.
+ * 
+ * Es similar al PDEstado, pero el Patron Strategy encapsula un algoritmo y no el estado de un contexto*/
 public class Strategy {
+
+	public interface Algoritmo{
+		public void moverse();
+	}
 	
-	public static void main(String[]args){
+	
+	public class EnTierra implements Algoritmo{
+		@Override 
+		public void moverse(){
+			System.out.println("Moviendose sobre ruedas");
+		}
+	}
+	
+	
+	public class EnAire implements Algoritmo{
+		@Override 
+		public void moverse(){
+			System.out.println("Volando en los aires");
+		}
+	}
+	
+	public class EnAireVeloz implements Algoritmo{
+		// Los override son una buena practica en la sobrescritura de los metodos.
+		@Override 
+		public void moverse(){
+			System.out.println( "Volando muy rapido!!");
+		}
+	}
+	
+	// Clase abstracta 
+	
+	public abstract class Avion { 
+		private Algoritmo miAlgoritmo;
 		
+		public void setAlgoritmo(Algoritmo a){
+			this.miAlgoritmo = a ;
+		}
+		
+		public void mover(){
+			this.miAlgoritmo.moverse();
+		}
+	}
+	
+	public class AvionComercial extends Avion{
+		public AvionComercial(){
+			
+		}
+	}
+	
+	public class AvionRapido extends Avion{
+		public AvionRapido(){
+			
+		}
 	}
 
 }
